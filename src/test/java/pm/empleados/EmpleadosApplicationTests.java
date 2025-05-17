d24e8265b75529043908798b4347b21430f7bf58
Pablo Mendez <pmendezpuentes@gmail.com>
2025-05-16 22:27:19 -0500

    Primer Commit

---
+++ src/test/java/pm/empleados/EmpleadosApplicationTests.java
@@ -0,0 +1,13 @@
+package pm.empleados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
